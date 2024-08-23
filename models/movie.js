7fd33714da54731149649b6a9c5ac80880f093fc
ahmed2704 <kassimahmed2704@gmail.com>
2024-08-23 11:35:44 -0400

    refactor review schema and movie schema

---
+++ models/movie.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      require: true,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      default: 1
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  reviews: [reviewSchema],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Movie", movieSchema);
